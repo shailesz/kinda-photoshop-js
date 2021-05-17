b8657e589d15b43048aaaf3fac5d1245673e1f37
shailesz <sailesh.karki@sagarmatha.edu.np>
2021-05-17 22:44:32 +0545

    load image complete

--- js/photoshop.js
+++ js/photoshop.js
@@ -4,13 +4,17 @@ var ctx = canvas.getContext("2d");
 var canvasDiv = document.querySelector(".canvas");
 var canvasDivHolder = document.querySelector(".canvas-holder");
 var menuBarFile = document.querySelector("#menu-bar-file");
+var fileButton = document.querySelector("#file-button");
+var newButton = document.querySelector("#file-button-dropdown-new");
+var openButton = document.querySelector("#file-button-dropdown-open");
+var openInput = document.querySelector("#file-open-input");
 canvasDiv.appendChild(canvas);
 
 canvas.offsetTop = 0;
 canvas.offsetLeft = 0;
 
 var image = new Image();
-image.src = "./images/2.png";
+// image.src = "./images/2.png";
 image.onload = () => {
   // taking image ko height width
   var imageWidth = image.width;
@@ -39,3 +43,38 @@ image.onload = () => {
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 };
 
+// trying to open files
+let showDropdown = () => {
+  let dropdown = document.querySelector("#file-button-dropdown");
+
+  if (dropdown.className === "menu-option-dropdown lsn") {
+    dropdown.className += " show";
+  } else {
+    dropdown.className = "menu-option-dropdown lsn";
+  }
+};
+
+// dropdown for file
+fileButton.addEventListener("click", showDropdown);
+
+newButton.addEventListener("click", () => {
+
+});
+
+// openInput
+openInput.addEventListener(
+  "change",
+  (e) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      console.log(reader.result);
+      image.src = reader.result;
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+      console.log(image.src);
+    };
+    reader.readAsDataURL(openInput.files[0]);
+    console.log(openInput.files[0]);
+  },
+  false
+);
